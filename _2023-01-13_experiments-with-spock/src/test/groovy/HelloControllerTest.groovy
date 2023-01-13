2e99a02c1d05391f50c08d24244737707ef49c44
Hannes Gerner <hannes.gerner@tngtech.com>
2023-01-13 14:07:56 +0100

    Add an example for a test with REST controller

---
+++ _2023-01-13_experiments-with-spock/src/test/groovy/HelloControllerTest.groovy
@@ -0,0 +1,25 @@
+import com.hg.sketchbook.spock.api.HelloController
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
+
+
+class HelloControllerTest extends Specification {
+    def helloController = new HelloController()
+
+    MockMvc mockMvc = standaloneSetup(helloController).build()
+
+    def "get response"() {
+        when: 'call is made'
+        def response = mockMvc.perform (get('/hello')).andReturn().response
+        def content = response.contentAsString
+
+        then:
+        response.status == OK.value()
+        content == "Hello world!"
+    }
+
+}
