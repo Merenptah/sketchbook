dd2f4272d65389a1f8231a345d2ed597c4e9c591
Hannes Gerner <hannes.gerner@gmail.com>
2023-01-29 15:20:44 +0100

    Step 3: Enforce a square of correct size

--- _2023-01-29_diamond-kata-with-spock/src/main/java/com/hg/sketchbock/spockdiamond/Diamond.java
+++ _2023-01-29_diamond-kata-with-spock/src/main/java/com/hg/sketchbock/spockdiamond/Diamond.java
@@ -1,6 +1,6 @@
 package com.hg.sketchbock.spockdiamond;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Diamond {
@@ -8,6 +8,17 @@ public class Diamond {
         if (c < 'A' || c > 'Z') {
             throw new IllegalArgumentException(c + " is outside the valid range A..Z");
         }
-        return Collections.singletonList("A");
+
+        final int size = (2 * (c - 'A')) + 1;
+        List<String> result = new ArrayList<>();
+        while (result.size() < size) {
+            StringBuilder row = new StringBuilder();
+            while (row.length() < size) {
+                row.append('A');
+            }
+            result.add(row.toString());
+        }
+
+        return result;
     }
 }
--- _2023-01-29_diamond-kata-with-spock/src/test/groovy/DiamondSpecification.groovy
+++ _2023-01-29_diamond-kata-with-spock/src/test/groovy/DiamondSpecification.groovy
@@ -4,12 +4,13 @@ import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
-import spock.util.matcher.HamcrestSupport
 
 class DiamondSpecification extends Specification {
     @Shared char aChar = 'A'
     @Shared char zChar = 'Z'
     @Shared Range<Character> validRange = aChar..zChar
+    @Shared char bChar = 'B'
+    @Shared Range<Character> testRange = bChar..zChar
 
     @Subject Diamond diamond = new Diamond()
 
@@ -30,5 +31,32 @@ class DiamondSpecification extends Specification {
     def "The diamond of A is 'A'"() {
         expect:
         diamond.apply(aChar) == ["A"]
+    }
+
+    @Unroll
+    def "diamond('#c') is square"() {
+        given:
+        def result = diamond.apply(c)
+
+        expect:
+        result.every {
+            it.length() == result.size()
+        }
+
+        where:
+        c << testRange
+    }
+
+    @Unroll("diamond(#c) should have #expectedHeight rows")
+    def "a diamond's height is determined by the character argument"() {
+        given:
+        def result = diamond.apply(c)
+
+        expect:
+        result.size() == expectedHeight
+
+        where:
+        c << testRange
+        expectedHeight = ((c - aChar) * 2) + 1
     }
 }
