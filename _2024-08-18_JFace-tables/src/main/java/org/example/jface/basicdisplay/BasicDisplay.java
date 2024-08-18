b242464220d1dce9490ae7de191204b40a14537d
Merenptah <8060706+Merenptah@users.noreply.github.com>
2024-08-18 09:31:17 +0200

    Add example of TableViewer for reference

--- _2024-08-18_JFace-tables/src/main/java/org/example/jface/basicdisplay/BasicDisplay.java
+++ _2024-08-18_JFace-tables/src/main/java/org/example/jface/basicdisplay/BasicDisplay.java
@@ -1,16 +1,16 @@
-package org.example.jface;
+package org.example.jface.basicdisplay;
 
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-public class Main {
+public class BasicDisplay {
     public static void main(String[] args) {
         Display display = new Display();
 
         Shell shell = new Shell(display);
         shell.setLayout(new FillLayout());
-
+ 
         shell.open();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch()) {
---
+++ _2024-08-18_JFace-tables/src/main/java/org/example/jface/tableviewerexample/README.md
@@ -0,0 +1,2 @@
+This example is taken from
+    https://www.eclipse.org/articles/Article-Table-viewer/table_viewer.html
